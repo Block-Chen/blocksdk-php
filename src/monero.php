03467ff2aa15860e3f8cacb832cff3883c59f4b4
blockchen-admin <61440698+blockchen-admin@users.noreply.github.com>
2020-07-17 15:54:27 +0900

    Update monero.php

--- src/monero.php
+++ src/monero.php
@@ -87,7 +87,7 @@
 			
 			$request['private_spend_key'] = isset($request['private_spend_key'])==false ?null:$request['private_spend_key'];
 			$request['password'] = isset($request['password'])==false ?null:$request['password'];
-			$request['subtractfeefromamount'] = isset($request['subtractfeefromamount'])==false ?flase:$request['subtractfeefromamount'];
+			$request['subtractfeefromamount'] = isset($request['subtractfeefromamount'])==false ?false:$request['subtractfeefromamount'];
 			
 			return $this->request("POST","/xmr/address/{$request['address_id']}/sendtoaddress",[
 				"address" => $request['address'],
