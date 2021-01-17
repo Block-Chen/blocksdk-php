10e205136d73844bab9f50a2757941b0161bcdca
blockchen-admin <61440698+blockchen-admin@users.noreply.github.com>
2021-01-17 17:13:10 +0900

    Add files via upload

--- src/token.php
+++ src/token.php
@@ -7,7 +7,7 @@
 		public function getUsages($request){
 			
 			return $this->request("GET","/token/usage",[
-				"stat_date" => $request['stat_date'],
+				"start_date" => $request['start_date'],
 				"end_date" => $request['end_date']
 			]);
 		}
