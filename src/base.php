db6345447b48ce59bc95e5b38d7d034765febf0a
blockchen-admin <61440698+blockchen-admin@users.noreply.github.com>
2021-01-17 16:43:21 +0900

    Add files via upload

--- src/base.php
+++ src/base.php
@@ -33,44 +33,14 @@
 			} 
 			
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);  
-			curl_setopt($ch, CURLOPT_HEADER, 1);
+			curl_setopt($ch, CURLOPT_HEADER, 0);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','x-api-key: '. $this->api_token));
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','x-api-token: '. $this->api_token));
 			$result = curl_exec($ch);
 			curl_close($ch);
 
 
-			$header_array = array();
-			$result_decode = array();
-			$result_row = explode("\n",$result);
-			for($i=0;$i < count($result_row);$i++){
-				$fields = explode(":",$result_row[$i]);
-				
-				if($i == 0){
-					$statusCode = explode(" ",$fields[0]);
-					$header_array['statusCode'] = empty($statusCode[1])?0:$statusCode[1];
-				}else if($i == count($result_row) - 1){
-					$result_decode = json_decode($result_row[$i],true);
-				}else if(isset($fields[1]) == false){
-					if(substr($fields[0], 0, 1) == "\t"){
-						end($header_array);
-						$header_array[key($header_array)] .= "\r\n\t".trim($fields[0]);
-					}else{
-						end($header_array);
-						$header_array[key($header_array)] .= trim($fields[0]);
-					}
-				}else if(empty($fields[1]) === false){
-					$field_title = trim($fields[0]);
-					if (!isset($header_array[$field_title])){
-						$header_array[$field_title]=trim($fields[1]);
-					}else if(is_array($header_array[$field_title])){
-						$header_array[$field_title] = array_merge($header_array[$fields[0]], array(trim($fields[1])));
-					}
-				}
-			}
-			
-			$result_decode['HTTP_HEADER'] = $header_array;
-			return $result_decode;
+			return json_decode($result,true);
 		}	
 	}
 ?>
