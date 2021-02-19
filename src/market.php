eff70d19619963622e298bcc0c77ec8b8c060f29
blockchen-admin <61440698+blockchen-admin@users.noreply.github.com>
2021-02-19 14:42:48 +0900

    Add files via upload

--- src/market.php
+++ src/market.php
@@ -8,6 +8,30 @@
 			
 			return $this->request("GET","/market/exchanges");
 		}
+		public function getAssets(){
+			
+			return $this->request("GET","/market/assets");
+		}
+		
+		public function getAsset($request){
+			return $this->request("GET","/market/assets/" . $request['asset_id']);
+		}
+		
+		public function getOhlcvLast($request){
+			return $this->request("GET","/market/ohlcv/latest",$request);
+		}
+		
+		public function getOhlcvHistory($request){
+			return $this->request("GET","/market/ohlcv/history/" . $request['symbol'],$request);
+		}		
+		
+		public function getExchangeOhlcvLast($request){
+			return $this->request("GET","/market/ohlcv/" . $request['exchage_id'] . "/latest",$request);
+		}
+		
+		public function getExchangeOhlcvHistory($request){
+			return $this->request("GET","/market/ohlcv/" . $request['exchange_id'] . "/history/" . $request['symbol'],$request);
+		}
 		
 		public function getTrades($request){
 			$request['from'] = isset($request['from'])==false ?null:$request['from'];
