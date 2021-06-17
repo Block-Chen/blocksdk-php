a53c2d5aa0bbb277e63025006c01d06a528aa4bd
blockchen-admin <61440698+blockchen-admin@users.noreply.github.com>
2021-06-17 11:28:15 +0900

    Update solc.php
    Add encodefunction

--- src/solc.php
+++ src/solc.php
@@ -7,5 +7,10 @@
 		public function genContract($request){
 			return $this->request("GET","/solc/{$request['net']}/{$request['type']}",$request);
 		}		
+		
+		public function encodefunction($request){
+			return $this->request("POST","/solc/encodefunction",$request);
+		}
+		
 	}
 ?>
